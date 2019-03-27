2a9f079bd7fb7b3bf5da41b27ef3019e8e0fcbf4
Alesh Houdek <mail@alesh.com>
2019-03-27 17:24:11 -0600

    Added Redux and set up store and rootReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import store from './store'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Provider store={store} >
+        <div className="App">
+        </div>
+      </Provider>
     );
   }
 }
